57e24ce985c9c15b73c5b4616c1b5aafa3fc3648
Miguel Tang <A01610836@itesm.mx>
2022-03-31 12:43:10 -0600

    Arreglar ruta de anuncio

--- routes/routes_anuncios.js
+++ routes/routes_anuncios.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
-const routes = express.Router();
+const router = express.Router();
 
 const controllerAnuncios = require('../controllers/controller_anuncios');
-const router = require('./routes_vista_general');
 
 router.get('/', controllerAnuncios.anuncios);
 
