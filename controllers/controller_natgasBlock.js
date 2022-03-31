e086f38b590e5e3b30c0ee7b5fc249620e74a4da
Miguel Tang <A01610836@itesm.mx>
2022-03-31 11:22:59 -0600

    Agregar html dinamico consultar sol ngb admin

--- controllers/controller_natgasBlock.js
+++ controllers/controller_natgasBlock.js
@@ -6,4 +6,5 @@ exports.solicitarNatgasBlock = (request, response, next) => {
 exports.solicitudesNatgasBlock = (request, response, next) => {
     console.log(request.body);
     console.log("Solicitudes de Natgas block");
+    response.render('consultarSolicitudesNGBAdmin');
 };
