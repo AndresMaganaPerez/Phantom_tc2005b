dbf80e85900d8c09d2a6b73e64b9a693f508414e
Miguel Tang <A01610836@itesm.mx>
2022-03-31 11:31:16 -0600

    Agregar html dinamico consultar solicitud ngb lider

--- controllers/controller_natgasBlock.js
+++ controllers/controller_natgasBlock.js
@@ -6,5 +6,5 @@ exports.solicitarNatgasBlock = (request, response, next) => {
 exports.solicitudesNatgasBlock = (request, response, next) => {
     console.log(request.body);
     console.log("Solicitudes de Natgas block");
-    response.render('consultarSolicitudesNGBAdmin');
+    response.render('consultarSolicitudesNGBLider');
 };
