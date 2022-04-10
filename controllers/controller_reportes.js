e02cb4036909635d316a7886375e915feff6ca5c
saidortigoza <a01707430@itesm.mx>
2022-04-09 21:41:16 -0500

    Vistas reportes iniciadas

--- controllers/controller_reportes.js
+++ controllers/controller_reportes.js
@@ -1,5 +1,15 @@
 exports.reportes = (request, response, next) => {
     console.log(request.body);
     console.log("Reportes");
+    response.render('consultarReportes');
 };
 
+exports.nuevo_reporte = (request, response, next) => {
+    console.log("Nuevo Reporte");
+    response.render('crearReporte');
+}
+
+exports.nuevo_reporte = (request, response, next) => {
+    console.log("Modificar Reporte");
+    response.render('modificarReporte');
+}
--- routes/routes_reportes.js
+++ routes/routes_reportes.js
@@ -7,4 +7,8 @@ const isAuth = require('../controllers/is-auth');
 
 router.get('/', isAuth, controllerReportes.reportes);
 
+router.get('/nuevo', isAuth, controllerReportes.nuevo_reporte);
+
+router.get('/modificar', isAuth, controllerReportes.modificar_reporte);
+
 module.exports = router;
