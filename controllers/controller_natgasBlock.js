d3456b6171827c766ebb2b4c32bd090fb956d80a
Miguel Tang <A01610836@itesm.mx>
2022-04-09 23:39:41 -0500

    Agregar vista consultar solNGB Admin

--- controllers/controller_natgasBlock.js
+++ controllers/controller_natgasBlock.js
@@ -7,5 +7,5 @@ exports.solicitarNatgasBlock = (request, response, next) => {
 exports.solicitudesNatgasBlock = (request, response, next) => {
     console.log(request.body);
     console.log("Solicitudes de Natgas block");
-    response.render('consultarSolicitudesNGBLider');
+    response.render('consultarSolicitudesNGBAdmin');
 };
