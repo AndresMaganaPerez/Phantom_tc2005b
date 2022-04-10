5a0aac375cc3a3633b5917d313f2ca5a772172f2
saidortigoza <a01707430@itesm.mx>
2022-04-10 09:00:48 -0500

    Reportes views ended

--- controllers/controller_reportes.js
+++ controllers/controller_reportes.js
@@ -1,5 +1,15 @@
 exports.reportes = (request, response, next) => {
     console.log(request.body);
     console.log("Reportes");
+    response.render('reportesAdmin');
 };
 
+exports.nuevo_reporte = (request, response, next) => {
+    console.log("Nuevo Reporte");
+    response.render('crearReporte');
+};
+
+exports.modificar_reporte = (request, response, next) => {
+    console.log("Modificar Reporte");
+    response.render('modificarReporte');
+};
