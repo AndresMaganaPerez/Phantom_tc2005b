e1097122fe386da861600ad26c630211a6885aeb
AlejandroRuizZ <a01611451@itesm.mx>
2022-04-11 23:15:37 -0500

    Desplegar Fecha Actual en Formulario, Fecha de Ultima Solicitud, Consultar Solicitudes Pendientes de BD Falta mandarlo a la vista

--- controllers/controller_natgasBlock.js
+++ controllers/controller_natgasBlock.js
@@ -1,15 +1,26 @@
 const NGB = require('../models/models_natgasBlock')
 
+var currentdate = new Date(); //ESTO TE DA LA FECHA ACTUAL
+var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear()
+
 exports.solicitarNatgasBlock = (request, response, next) => {
     console.log(request.body);
     console.log("Solicitar mi Natgas Block");
+    NGB.getUltimaSolcitud(request.session.empleado.idEmpleado)
+    .then(([rows,fieldData]) =>{
+        const ultimoNGB = rows[0].fechaSolicitud
     response.render('natgasBlock/nuevaSolicitudNGB', {
         sesion: request.session.empleado,
         rol: request.session.rol,
         privilegios: request.session.privilegios,
-        restantes: request.session.empleado.cantidadNatgasBlocks
-
+        restantes: request.session.empleado.cantidadNatgasBlocks,
+        fechaDeHoy: datetime,
+        ultimoNGB: ultimoNGB
     })
+    }).catch(err =>{console.log(err)})
+    
 };
 exports.postDeSolicitud = (request,response,next) => {
     const ngb = new NGB(request.session.empleado.idEmpleado,request.body.fecha)
@@ -20,21 +31,25 @@ exports.postDeSolicitud = (request,response,next) => {
 }
 
 exports.solicitudesAceptarNatgasBlock = (request, response, next) => {
-    console.log(request.body);
-    console.log("Solicitudes para Aceptar de Natgas block");
-    response.render('natgasBlock/aceptarEstatusSolicitudesNGB', {
-        sesion: request.session.empleado,
-        rol: request.session.rol,
-        privilegios: request.session.privilegios
-    });
+    NGB.getNGBDeMisEmpleados(request.session.empleado.idEmpleado)
+    .then(([rows,fieldData]) =>{
+        console.log(request.body);
+        console.log("Solicitudes para Aceptar de Natgas block");
+        response.render('natgasBlock/aceptarEstatusSolicitudesNGB', {
+            ngbsPorAceptar: rows,
+            sesion: request.session.empleado,
+            rol: request.session.rol,
+            privilegios: request.session.privilegios
+        });
+    }).catch(err =>{console.log(err)})
 
 };
 
 exports.solicitudesEstatusNatgasBlock = (request, response, next) => {
-    console.log("Consultar Solicitudes NGB");
+        console.log("Consultar Solicitudes NGB");
     response.render('natgasBlock/estatusSolicitudesNGB', {
         sesion: request.session.empleado,
         rol: request.session.rol,
         privilegios: request.session.privilegios
-    });
+    });   
 };
--- models/models_natgasBlock.js
+++ models/models_natgasBlock.js
@@ -6,7 +6,7 @@ module.exports = class NGB {
         this.fecha = _fecha;
     }
     save_NGB() {
-        return db.execute('INSERT INTO natgasblocks (empleadoID,fechaSolicitud) VALUES (?,?)',
+        return db.execute('INSERT INTO natgasblocks (idEmpleado,fechaSolicitud) VALUES (?,?)',
         [this.nomina,this.fecha])
     }
     /*static getNGBRestantes(_nomina){
@@ -14,5 +14,11 @@ module.exports = class NGB {
     }*/
     static RestaNGBRestantes(_nomina){
         return db.execute('UPDATE empleado SET cantidadNatgasBlocks = cantidadNatgasBlocks - 1 WHERE idEmpleado =?',[_nomina])
+    }
+    static getUltimaSolcitud(_nomina){
+        return db.execute('SELECT fechaSolicitud FROM natgasblocks WHERE idEmpleado =? ORDER BY fechaSolicitud DESC LIMIT 1',[_nomina])
+    }
+static getNGBDeMisEmpleados(_nomina){
+        return db.execute('SELECT * FROM natgasblocks WHERE idEmpleado IN (SELECT idOperador FROM DIRIGE D WHERE idLider =?)',[_nomina])
     }
 }
