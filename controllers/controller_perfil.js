7c875be48aec6995f80876c21f54ba715ae7c627
Miguel Tang <A01610836@itesm.mx>
2022-04-15 16:34:52 -0500

    Agregar bloques/componentes a vista perfil

--- controllers/controller_perfil.js
+++ controllers/controller_perfil.js
@@ -1,7 +1,6 @@
-
 exports.perfil = (request, response, next) => {
     console.log(request.body);
-    response.render('perfil/vistaPerfilAdmin',{
+    response.render('perfil/vistaPerfil',{
         sesion: request.session.empleado,
         rol: request.session.rol,
         privilegios: request.session.privilegios
