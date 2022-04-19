38fd3732b4f6867e22194b9436b44d3d5599c3ac
Andrés Magaña Pérez <a01067963@itesm.mx>
2022-04-18 22:45:26 -0500

    General Banners

--- controllers/controller_vista_general.js
+++ controllers/controller_vista_general.js
@@ -1,11 +1,20 @@
+const Banners = require('../models/models_banners');
+
 exports.vistaGeneral = (request, response, next) => {
-    console.log(request.body);
-    console.log(request.session.empleado);
-    console.log(request.session.rol);
-    console.log(request.session.privilegios);
-    response.render('general/general', {
-        sesion: request.session.empleado,
-        rol: request.session.rol,
-        privilegios: request.session.privilegios
-    });
+    Banners.fetchAllBanners()
+    .then(([rows, fieldData]) => {
+        const banners = rows;
+        console.log(request.body);
+        console.log(request.session.empleado);
+        console.log(request.session.rol);
+        console.log(request.session.privilegios);
+        response.render('general/general', {
+            sesion: request.session.empleado,
+            rol: request.session.rol,
+            privilegios: request.session.privilegios,
+            banners: banners
+        });
+    }).catch((err) => {
+            console.log(err);
+        });
 };
