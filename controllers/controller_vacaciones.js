6b661fa55a2ecf3c5441c877c26064024a1cdf7e
saidortigoza <a01707430@itesm.mx>
2022-04-23 17:45:48 -0500

    Filtro vacaciones mes 2.0

--- controllers/controller_vacaciones.js
+++ controllers/controller_vacaciones.js
@@ -407,41 +407,16 @@ exports.estatusVacaciones = (request, response, next) => {
 exports.filtraSolVacaciones = (request, response, next) => {
     // Variable correspondiente a mes o area
     const monar = request.params.mesar;
-    let mes = 0;
 
-    if (monar == 'Enero') {
-        mes = 01;
-    } else if (monar == 'Febrero') {
-        mes = 02;
-    } else if (monar == 'Marzo') {
-        mes = 03;
-    } else if (monar == 'Abril') {
-        mes = 04;
-    } else if (monar == 'Mayo') {
-        mes = 05;
-    } else if (monar == 'Junio') {
-        mes = 06;
-    } else if (monar == 'Julio') {
-        mes = 07;
-    } else if (monar == 'Agosto') {
-        mes = 08;
-    } else if (monar == 'Septiembre') {
-        mes = 09;
-    } else if (monar == 'Octubre') {
-        mes = 10;
-    } else if (monar == 'Noviembre') {
-        mes = 11;
-    } else if (monar == 'Diciembre') {
-        mes = 12;
-    }
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    let index_mes = meses.indexOf(monar) + 1;
 
     // Variable que obtiene el año actual
     const y = new Date();
     let year = y.getFullYear();
-    console.log(mes);
-    console.log(year);
 
-    Solicitudes.filtraSolVacacionesMes(year, mes, monar)
+    Solicitudes.filtraSolVacacionesMes(year, index_mes, monar)
         .then(([rows, fieldData]) => {
             Solicitudes.fetchAreas()
                 .then(([areas, fieldData]) => {
