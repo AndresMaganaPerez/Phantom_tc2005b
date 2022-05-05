5847326ea507ab874d3bca9ce4ef3b4f6cf842a3
saidortigoza <a01707430@itesm.mx>
2022-05-05 12:18:47 -0500

    Fix errors

--- controllers/controller_anuncios.js
+++ controllers/controller_anuncios.js
@@ -40,8 +40,6 @@ exports.nuevoAnuncio = (request, response, next) => {
     let dateStr = date.getFullYear() + '-' + ("0" + mes).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
     const nuevaFecha = new Date(dateStr);
     
-    console.log(dateStr);
-    
     response.render('anuncios/crearAnuncio',{
         sesion: request.session.empleado,
         rol: request.session.rol,
@@ -54,14 +52,23 @@ exports.nuevoAnuncio = (request, response, next) => {
 
 exports.postAnuncio = (request, response, next) => {
     let date= new Date();
-    let today = Date.now();
+    date.setMonth(date.getMonth() + 2);
     let mes = date.getMonth() + 1;
     let dateStr = date.getFullYear() + '-' + ("0" + mes).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
+    
+    let fActual = new Date();
+    let month = fActual.getMonth() + 1;
+    let aux_f = fActual.getFullYear() + '-' + ("0" + month).slice(-2) + '-' + ("0" + fActual.getDate()).slice(-2);
+
+    console.log(aux_f);
+
+    console.log(request.body.expiracion);
+    
     const elim = 0;
     
     const anuncio = new Anuncio(dateStr, request.body.titulo, request.body.pin, request.body.expiracion, request.body.texto, request.file ? request.file.filename : null, elim);
 
-    if (request.body.expiracion > dateStr) {
+    if (request.body.expiracion > aux_f) {
         const flag = 'success';
 
         anuncio.saveAnuncio()
@@ -71,6 +78,7 @@ exports.postAnuncio = (request, response, next) => {
                     rol: request.session.rol,
                     privilegios: request.session.privilegios,
                     fechaDeHoy: datetime,
+                    fechaDefault: dateStr,
                     flag: flag
                 });
             }).catch(err => {
@@ -84,13 +92,13 @@ exports.postAnuncio = (request, response, next) => {
         let mes = date.getMonth() + 1;
         let dateStr = date.getFullYear() + '-' + ("0" + mes).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
         
-        console.log(dateStr);
         response.render('anuncios/crearAnuncio', {
             sesion: request.session.empleado,
             rol: request.session.rol,
             privilegios: request.session.privilegios,
             expiracion: request.body.expiracion,
             fechaDeHoy: datetime,
+            fechaDefault: dateStr,
             flag: flag
         });
     }
@@ -114,6 +122,14 @@ exports.modificarAnuncio = (request, response, next) => {
         if (request.body.pin == 'on') {
             pin_status = 1;
         }
+
+        console.log(idA);
+    console.log(idR);
+    console.log(request.body.titulo);
+    console.log(pin_status);
+    console.log(request.body.expiracion);
+    console.log(request.body.texto);
+    console.log(request.file);
 
         Anuncio.modificarAnuncio(idA, idR, dateStr, request.body.titulo, pin_status, request.body.expiracion, request.body.texto, request.file ? request.filename: null).then(() => {
             Anuncio.fetchAllPinned(dateStr).then(([rowsPin, fieldData]) => {
@@ -187,6 +203,7 @@ exports.eliminarAnuncio = (request, response, next) => {
                     anunciosPinned: rowsPin,
                     anunciosUnpinned: rowsUnpin,
                     hoy: dateStr,
+                    fechaDeHoy: dateStr,
                     flag: flag
                 });
             }).catch((error) => {
